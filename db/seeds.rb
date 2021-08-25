d9b87e76b87be953d2738032d3009abbd7ce5776
Holly Wrench <holly.wrench@gmail.com>
2021-08-25 12:56:12 +0100

    began creating seed, added cloudinary code to events index page to display photo, added a has_many/belongs_to relationship to the user/event, and has_many :photos to events model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+
+file1 = URI.open('https://dwj199mwkel52.cloudfront.net/assets/core/press/portrait-sebastien-b27d9f62bd92411b2838dce94e12f59b3999ba6dd43a38234fd68ee302700ba3.jpg') # seb
+file2 = URI.open('https://miro.medium.com/max/11520/0*u9cIet_TY8gODSKf') # panda
+event = Event.new(name: "Dream")
+event.photo.attach(io: file1, filename: 'image.png', content_type: 'image/png')
+event.photo.attach(io: file2, filename: 'image.png', content_type: 'image/png')
+event.save
