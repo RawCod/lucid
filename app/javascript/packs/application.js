bf9a666eee1b21326aa0781d926639194f83889b
Susheel Shivaprasad <susheelshivaprasad@gmail.com>
2021-08-27 15:56:02 +0100

    sweet alert moved inside of turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import { initSweetalert } from '../plugins/init_sweetalert';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -31,18 +32,17 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initSweetalert('#sweet-alert-button', {
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your event suggestion has been submitted',
+    text: 'We will contact you if it is selected as a future event',
+    timer: 5000
+  }, () => {
+    window.location.replace("/events");
+  });
 });
 
-import { initSweetalert } from '../plugins/init_sweetalert';
 
-initSweetalert('#sweet-alert-button', {
-  position: 'top-end',
-  icon: 'success',
-  title: 'Your event suggestion has been submitted',
-  text: 'We will contact you if it is selected as a future event',
-  timer: 5000
-}, () => {
-  window.location.replace("/events");
-});
 
 // can't use redirect_to in javascript - isn't a js function - instead window.location.replace()
